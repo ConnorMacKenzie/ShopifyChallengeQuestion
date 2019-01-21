e8bbf9df7e3b0767d8e30981c1e698e12fabe1eb
ConnorMacKenzie <connormack97@gmail.com>
2019-01-21 02:11:19 -0500

    Production config for Heroku

--- lib/products_api_web/endpoint.ex
+++ lib/products_api_web/endpoint.ex
@@ -2,7 +2,7 @@ defmodule ProductsApiWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :products_api
 
   socket "/socket", ProductsApiWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   # Serve at "/" the static files from "priv/static" directory.
